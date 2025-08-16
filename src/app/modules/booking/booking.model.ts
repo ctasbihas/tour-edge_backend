062c3cf99b3c2ceac791b3faf2bc11dd7acdc977
ctasbihas <ctasbihas@gmail.com>
2025-08-16 22:34:36 +0600

    feat: implement booking model with schema

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import { BookingStatus, IBooking } from "./booking.interface";
+
+const bookingSchema = new Schema<IBooking>({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	tour: {
+		type: Schema.Types.ObjectId,
+		ref: "Tour",
+		required: true,
+	},
+	guestCount: {
+		type: Number,
+		required: true,
+	},
+	payment: {
+		type: Schema.Types.ObjectId,
+		ref: "Payment",
+		required: true,
+	},
+	status: {
+		type: String,
+		enum: Object.values(BookingStatus),
+		default: BookingStatus.PENDING,
+	},
+});
+
+export const Booking = model<IBooking>("Booking", bookingSchema);
